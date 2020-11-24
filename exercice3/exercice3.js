7405d9db47b95c5efde0e1be02bb9488025e7726
Clems-web <clement.servais1@gmail.com>
2020-11-24 14:19:33 +0100

    exo 1, 2 et 3 finis

--- exercice3/exercice3.js
+++ exercice3/exercice3.js
@@ -1,10 +1,10 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
-
-if()
-{
-
+let x = 5+3;
+let y = 5;
+if(x === y) {
+    document.getElementById('monDiv').innerHTML = "x est égal à y";
 }
 
-{
-
+else {
+    document.getElementById('monDiv').innerHTML = "x n'est pas égal à y";
 }
